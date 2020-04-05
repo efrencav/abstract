14dab82c77bfde640a590ab8164afdea5846c5a1
efrencav <efren@abstract-creative.com>
2020-04-05 15:41:20 -0500

    greeting

---
+++ assets/js/greeting.js
@@ -0,0 +1,13 @@
+const today = new Date();
+const hourNow = today.getHours();
+let greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
