d59bcd816856eb6ebc7c1460cc9798e36b6cc6a1
efrencav <efren@abstract-creative.com>
2020-04-05 17:20:24 -0500

    update images

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -1,13 +1,15 @@
-const today = new Date();
-const hourNow = today.getHours();
-let greeting;
+// const today = new Date();
+// const hourNow = today.getHours();
+// let greeting;
 
-if (hourNow > 18) {
-    greeting = 'Good evening!';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-} else if (hourNow > 0) {
-    greeting = 'Good morning!';
-} else {
-    greeting = 'Welcome!';
-}
+// if (hourNow > 18) {
+//     greeting = 'Good evening!';
+// } else if (hourNow > 12) {
+//     greeting = 'Good afternoon!';
+// } else if (hourNow > 0) {
+//     greeting = 'Good morning!';
+// } else {
+//     greeting = 'Welcome!';
+// }
+// document.getElementById("greeting").innerHTML`<p>` + greeting + `</p>`;
+
