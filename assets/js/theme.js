5efb21fe8ce0780300012dd5a158996c116b7087
Efrencav <efren.cavazos@gmail.com>
2020-06-07 12:20:32 -0500

    greeting

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1020,3 +1020,13 @@
     }
   })();
 })(jQuery);
+
+var greet = [
+  'What are you doing that early?',
+  'Good Morning',
+  'Good Afternoon',
+  'Good Evening'
+][ parseInt(new Date().getHours() / 24 * 4) ];
+
+
+document.getElementById("greeting").innerHTML = greet;
